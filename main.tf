99ddde821ecdbcf332fb3a1b6765e4615c5ba71a
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2025-09-25 09:42:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
-variable "name"{
-default = "testing-sensitivity"
-sensitive=true
-}
-
-
 resource "null_resource" "test"{
 count =2
 }
 resource "null_resource" "test1"{
+}
+output "resource-name" {
+  value = null_resource.test.id
+  sensitive = true
 }
