d2535223fa1052f0fcda9b232ed966d283eb7e47
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2025-09-25 09:44:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ resource "null_resource" "test"{
 resource "null_resource" "test1"{
 }
 output "resource-name" {
-  value = null_resource.test.id
+  value = null_resource.test.name
   sensitive = true
 }
