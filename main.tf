e8fe2ac9f9d8c7c414596aaf9725e99a082a7688
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2026-03-19 12:46:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,5 @@ provider "local" {}
 provider "kubernetes" {}
 provider "null" {}
 resource "null_resource" "test"{}
-terraform {
-required_providers {
-          argocd= { 
-            source = "argoproj-labs/argocd" 
-          } 
-          } 
-          } 
-provider "argocd" {}
+
 
